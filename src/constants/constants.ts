375cd3f857256f5e453b839460394a6d90d67a8f
Kellen Blumberg <kellen@Kellens-MacBook-Pro.local>
2023-05-03 17:17:56 -0700

    “updates”

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,9 +5,9 @@ export const AWS_SECRET = process.env.REACT_APP_AWS_SECRET;
 
 export const isDev = false;
 
-// export const BACKEND_URL = 'https://trails-protocol-backend.herokuapp.com';
+export const BACKEND_URL = 'https://trails-protocol-backend.herokuapp.com';
 // export const BACKEND_URL = 'https://trails-protocol-backend-dev.herokuapp.com';
-export const BACKEND_URL = 'http://localhost:5000';
+// export const BACKEND_URL = 'http://localhost:5000';
 // export const BACKEND_URL: string = isDev ? 'http://localhost:5000' : 'https://trails-protocol-backend.herokuapp.com';
 
 // export const NETWORK = isDev ? 'https://api.devnet.solana.com' : 'https://red-cool-wildflower.solana-mainnet.quiknode.pro/a1674d4ab875dd3f89b34863a86c0f1931f57090/';
